eb2f5a8a90e5a3c3ab18417425d9af6456251a14
CDaye <c.daye@hotmail.co.uk>
2015-06-17 23:24:09 +0100

    [CD] Setup project and added CPU class

---
+++ virtualmachine/virtualmachine/CPU.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualmachine
+{
+
+    class CPU
+    {
+        public void Tick()
+        {
+            int a = 0;
+        }
+    }
+}
---
+++ virtualmachine/virtualmachine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualmachine
+{
+    class Program
+    {
+        static CPU m_cpu;
+        static void Main(string[] args)
+        {
+            m_cpu = new CPU();
+            bool running = true;
+            while (running)
+            {
+                m_cpu.Tick();
+            }
+        }
+    }
+}

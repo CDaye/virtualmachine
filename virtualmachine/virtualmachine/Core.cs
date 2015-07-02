a33286dacf2472174a229270a702c0b25d9167fa
CDaye <c.daye@hotmail.co.uk>
2015-07-02 22:28:08 +0100

    added fetch unit, and pipeline stage enum

--- virtualmachine/virtualmachine/Core.cs
+++ virtualmachine/virtualmachine/Core.cs
@@ -8,9 +8,32 @@ namespace virtualmachine
 {
     class Core
     {
-        public void Tick()
+       
+        public uint m_isntructionPointer;
+        public PipelineStages m_pipelineStage;
+        InstructionFetchUnit m_fetchUnit;
+        public PipelineStages m_nextStage; 
+
+        public Core()
         {
-            int a = 0;
+            m_isntructionPointer = 0;
+            m_pipelineStage = PipelineStages.InstructionFetch;
+            m_fetchUnit = new InstructionFetchUnit();
+            m_nextStage = PipelineStages.InstructionFetch;
+           
         }
+                public void Tick()
+        {
+            m_fetchUnit.Tick();
+            m_pipelineStage = m_nextStage;
+        }
+    }
+    enum PipelineStages
+    {
+        BranchPredict,
+        InstructionFetch,
+        InstructionDispatch,
+        Execution,
+        Retirement
     }
 }
